be9b534610c8083068cf835836450d683d15bc3a
Juan <jdeluca@itba.edu.ar>
2023-06-16 20:00:47 -0300

    feat: continued main and recover
    Co-authored-by: skonfederak <solkonfe@users.noreply.github.com>
    Co-authored-by: Milagros Cornidez <mcornidez@users.noreply.github.com>
    Co-authored-by: Florencia Chao <florchao@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -3,81 +3,81 @@
 #include "../include/secret.h"
 #include "../include/utils.h"
 
-void printHelp(char * program);
-void checkValidK(char * k, struct * params);
+void printHelp(char* program);
+void checkValidK(char* k, struct* params);
+struct params* initParams(int argc, char* argv[]);
 
 
-struct params * initParams(int argc, char * argv[]){
+int main(int argc, char* argv[]) {
 
-    
-    struct params * params = malloc(sizeof (struct params));
-
-    if (strcmp(argv[1], "-r") == 0){
-        params -> action = RECOVER;
+    if (argc < 2) {
+        printHelp(argv[0]);
+        return 1;
     }
-    else if (strcmp(argv[1], "-d") == 0 ){
-        params-> action = DISTRIBUTE;
+
+    struct params* params = initParams(argc, argv);
+
+    if (params->action == DISTRIBUTE) {
+        distribute();
+    }
+    else if (params->action == RECOVER) {
+        recover();
     }
     else {
-         fprintf(stderr, "error, invalid action ");
-         exit (1);
-    }
-
-    params -> file = malloc(strlen(argv[2])); 
-    strcpy(params-> file, argv[2]);
-
-    checkValidK(argv[3], params);
-
-    params-> directory = malloc(strlen(argv[4]));
-    strcpy(params-> directory, argv[4]);
-    params -> n = SHADOWS;
-    return params;
-}
-
-
-int main (int argc, char * argv[]){
-
-    if (argc < 2){
-            printHelp(argv[0]);
-            return 1;
-    }
-    
-    struct params * params = initParams(argc, argv);
-
-    if (params -> action == DISTRIBUTE){
-        distribute();
-    } else if (params-> action == RECOVER){
-        recover();
-    } else {
-         fprintf(stderr, "error, invalid action ");
-         return 1;
+        fprintf(stderr, "error, invalid action ");
+        return 1;
     }
 
     return 0;
-
-
 }
 
-void checkValidK(char * k, struct * params){
-    int aux = atoi(k);
-    if (aux < 3 || aux > 8){
-         fprintf(stderr, "the k selected, is not valid");
-         exit(1);
+struct params* initParams(int argc, char* argv[]) {
+
+    struct params* params = malloc(sizeof(struct params));
+
+    if (strcmp(argv[1], "-r") == 0) {
+        params->action = RECOVER;
     }
-    params -> k = atoi(argv[3]);
+    else if (strcmp(argv[1], "-d") == 0) {
+        params->action = DISTRIBUTE;
+    }
+    else {
+        fprintf(stderr, "error, invalid action ");
+        exit(1);
+    }
 
+    params->file = malloc(strlen(argv[2]));
+    strcpy(params->file, argv[2]);
+
+    checkValidK(argv[3], params);
+
+    params->directory = malloc(strlen(argv[4]));
+    strcpy(params->directory, argv[4]);
+    params->n = SHADOWS;
+    return params;
 }
 
-void printHelp(char * program){
+void checkValidK(char* k, struct* params) {
+    int aux = atoi(k);
+    if (aux < 3 || aux > 8) {
+        fprintf(stderr, "the k selected, is not valid");
+        exit(1);
+    }
+    params->k = aux;
+}
+
+
+void printHelp(char* program) {
     fprintf(stderr,
-                    "usage: %s [-d | -r] [options]...\n"
-                    "\n"
-                    "   -d                                     indicates that a secret image will be distributed among other images.\n"
-                    "   -r                                     indicates that a secret image will be recovered from other images.\n"
-                    "   -k [number]                            minimum amount of shadows required to recover the secret in a (k, n) scheme.\n"
-                    "   -i [path]                              the name of a .bmp file.\n"
-                    "                                                - when using -d this file must already exist as it is the image to be hidden.\n"
-                    "                                                - when using -r this file will be the output file with the revealed secret image at the end of the program.\n"
-                    "   -o [path]                              the directory where the images will be distributed with the secret (if option (d) was chosen) or where the images containing the hidden secret are located (if option (r) was chosen).\n"
-                    ,program);
+        "usage: %s [-d | -r] [options]...\n"
+        "\n"
+        "   -d                                     indicates that a secret image will be distributed among other images.\n"
+        "   -r                                     indicates that a secret image will be recovered from other images.\n"
+        "   -k [number]                            minimum amount of shadows required to recover the secret in a (k, n) scheme.\n"
+        "   -i [name]                              the name of a .bmp file.\n"
+        "                                                - when using -d this file must already exist as it is the image to be hidden.\n"
+        "                                                - when using -r this file will be the output file with the revealed secret image at the end of the program.\n"
+        "   -o [path]                              - when using -d the directory where the images will be distributed with the secret.\n"
+        "                                          - when using -r where the images containing the hidden secret are located.\n"
+        , program);
 }
--- src/secret.c
+++ src/secret.c
@@ -1,83 +1,93 @@
 #include "../include/secret.h"
 
-bool validCoefficients (Poly * a, Poly * b);
+bool validCoefficients(Poly* a, Poly* b);
 
 
-void recover(struct * params){
-    uint8_t ** shadows = initShadows(n, size); //malloc de shadows
-    bmpFile * currentImageFile;
-    for (int i=0; i < params->k; i++){
-        //funcion openDirectory
+void recover(struct* params) {
+
+    char** fileNames = makePath(params);
+    uint8_t** shadows = initShadows(n, size); //malloc de shadows
+    bmpFile* currentImageFile;
+    for (int i = 0; i < params->k; i++) {
         currentImageFile = openBmpFile(params->directory);
-
+        shadows[i] = fromImageToShadow(params->k, currentImageFile);
     }
+
+    bmpFile* file = malloc(sizeof(bmpFile*));
+    int size = currentImageFile->header->size - currentImageFile->header->image_size_bytes;
+    file->header = malloc(size * sizeof(uint8_t));
+    memcpy(file->header, currentImageFile->header, size);
+    file->pixels = calloc(file->header->image_size_bytes, 1);
 
 }
 
 //https://c-for-dummies.com/blog/?p=3246#:~:text=The%20directory%20holds%20all%20that,It%27s%20prototyped%20in%20the%20dirent.
-void makePath(struct * params){
-    DIR * directory = openDir();
-    if (directory == NULL){
+char** makePath(struct* params) {
+    DIR* directory = openDir();
+    if (directory == NULL) {
         return;
     }
-    struct dirent *entry;
-    int files = 0;
-    while( (entry=readdir(directory)) )
-    {
-        if(strcmpy(entry->d_name, ".") == 0 || strcmpy(entry->d_name, "..") == 0){
+
+    char** fileNames = malloc(params->n * sizeof(char*));
+    struct dirent* entry;
+    int file = 0;
+    while ((entry = readdir(directory))) {
+        if (strcmpy(entry->d_name, ".") == 0 || strcmpy(entry->d_name, "..") == 0) {
             continue;
         }
-        
-        files++;
-        printf("File %3d: %s\n",
-                files,
-                entry->d_name
-              );
+        uint64_t directoryLength = strlen(params->directory);
+        uint64_t fileNameLength = directoryLength + 1 + strlen(entry->d_name);
+        fileNames[file] = malloc(fileNameLength + 1); // Add 1 for null terminator
+
+        strcpy(fileNames[file], params->directory);
+        strcat(fileNames[file], "/");
+        strcat(fileNames[file], entry->d_name);
+
+        file++;
     }
 
-    closedir(folder);
-
-
+    return fileNames;
 }
 
-uint8_t ** initShadows (int n, int size){
-    uint8_t** shadows = (uint8_t ** )malloc(sizeof (uint8_t *) * n);
+uint8_t** initShadows(int n, int size) {
+    uint8_t** shadows = (uint8_t**) malloc(sizeof(uint8_t*) * n);
     //shadow[shadowNumber][pointNumber] = point
-    for (int i=0 ; i < n ; i++){
-        shadows[i] = (uint8_t*)malloc(sizeof(uint8_t) * size);
+    for (int i = 0; i < n; i++) {
+        shadows[i] = (uint8_t*) malloc(sizeof(uint8_t) * size);
     }
     return shadows;
 }
 
 
-uint8_t * getSecret(uint8_t ** shadows, int * x_values, int length, int k) {
-    int secretLength = length * (k - 1);shadad00/cripto/src/utils.c
+uint8_t* getSecret(uint8_t** shadows, int length, int k, bmpFile* file; ) {
+    int secretLength = length * (k - 1);
+    uint8_t* imagePointer = file->pixels;
     int blockSize = BLOCK_SIZE(k);
-    uint8_t * secret = (uint8_t*)malloc(sizeof(uint8_t) * secretLength);
+    uint8_t* secret = (uint8_t*) malloc(sizeof(uint8_t) * secretLength);
     int current = 0;
 
-    for (int i=0; i < length; i+=2, current++){
+    for (int i = 0; i < length; i += 2, current++) {
         //par
         int ys1[k];
         //impar
         int ys2[k];
 
-        for (int j=0; j< k ; j++){
+        for (int j = 0; j < k; j++) {
             ys1[j] = shadows[j][current * 2];
             ys2[j] = shadows[j][current * 2 + 1];
         }
 
-        Poly * a = interPoly(k, x_values, ys1);
-        Poly * b = interPoly(k, x_values, ys2);
+        Poly* a = interPoly(k, x_values, ys1);
+        Poly* b = interPoly(k, x_values, ys2);
 
-        if (!validCoefficients(a, b)){
+        if (!validCoefficients(a, b)) {
             return NULL;
         }
 
-        for (int t=0; t< k ; t++){
+        for (int t = 0; t < k; t++) {
             secret[current * blockSize + t] = a->coef[t];
-            if( t >= 2){
-                secret[current * blockSize + k + t -2] = b->coef[t];
+            if (t >= 2) {
+                secret[current * blockSize + k + t - 2] = b->coef[t];
             }
         }
 
@@ -87,35 +97,35 @@ uint8_t * getSecret(uint8_t ** shadows, int * x_values, int length, int k) {
     return secret;
 }
 
-void generateShadows(uint8_t * secret, int length, int k, int n){
+void generateShadows(uint8_t* secret, int length, int k, int n) {
     int blockSize = BLOCK_SIZE(k);
-    int size = length / (k-1);
-    uint8_t ** shadows = initShadows(n, size);
+    int size = length / (k - 1);
+    uint8_t** shadows = initShadows(n, size);
 
     //divide secret
     int shadowPoints = 0;
-    for (int i=0; i < length; i+=size, shadowPoints+=2){
+    for (int i = 0; i < length; i += size, shadowPoints += 2) {
         int auxa0 = secret[i];
         int auxa1 = secret[i + 1];
 
         secret[i] = TO_POSITIVE(secret[i]) ? secret[i] : 1;
         secret[i + 1] = TO_POSITIVE(secret[i + 1]) ? secret[i + 1] : 1;
 
-        Poly * a = polyFromBytes(k, &secret[i]);
-        Poly * b = polyFromBytes(k, &secret[i + k -2]);
+        Poly* a = polyFromBytes(k, &secret[i]);
+        Poly* b = polyFromBytes(k, &secret[i + k - 2]);
 
         uint8_t random = (rand() % (MOD - 2)) + 1;
 
-        b->coef[0] = TO_POSITIVE(- (TO_POSITIVE(random) * secret[i]));
-        b->coef[1] = TO_POSITIVE(- (TO_POSITIVE(random) * secret[i+1]));
+        b->coef[0] = TO_POSITIVE(-(TO_POSITIVE(random) * secret[i]));
+        b->coef[1] = TO_POSITIVE(-(TO_POSITIVE(random) * secret[i + 1]));
 
         secret[i] = auxa0;
-        secret[i+1] = auxa1;
+        secret[i + 1] = auxa1;
 
         //save shadows 
-        for (int j = 0; j < n; j++){
-            shadows[j][shadowPoints] = evaluatePolynomial(a,j+1);
-            shadows[j][shadowPoints + 1] = evaluatePolynomial(b,j+1);
+        for (int j = 0; j < n; j++) {
+            shadows[j][shadowPoints] = evaluatePolynomial(a, j + 1);
+            shadows[j][shadowPoints + 1] = evaluatePolynomial(b, j + 1);
         }
 
         freePoly(a);
@@ -128,9 +138,9 @@ void generateShadows(uint8_t * secret, int length, int k, int n){
 //--------HELPER FUNCTIONS-------------------------
 //-------------------------------------------------
 
-bool validCoefficients (Poly * a, Poly * b) {
-    for (int i=0; i < MOD; i++ ){
-        if (TO_POSITIVE(a->coef[0] * i + b->coef[0]) == 0 && TO_POSITIVE(a->coef[1] * i + b->coef[1]) == 0){
+bool validCoefficients(Poly* a, Poly* b) {
+    for (int i = 0; i < MOD; i++) {
+        if (TO_POSITIVE(a->coef[0] * i + b->coef[0]) == 0 && TO_POSITIVE(a->coef[1] * i + b->coef[1]) == 0) {
             return true;
         }
     }
