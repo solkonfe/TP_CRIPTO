972162f5126d83975b073cf403f02def1f0770b0
florencia chao <fchao@itba.edu.ar>
2023-06-16 09:46:42 -0300

    added images

--- include/steganography.h
+++
@@ -1,9 +0,0 @@
-#ifndef STEGANOGRAPHY_H
-#define STEGANOGRAPHY_H
-#include <stdint.h>
-
-uint8_t * hide(uint8_t * secret, int secretLength, uint8_t * originalBytes, int bitsPerByte);
-
-uint8_t * read(uint8_t * hidden, int secretLength, int bitsPerByte, uint8_t * output);
-
-#endif // STEGANOGRAPHY_H
